ad4b9ed6bfc89d9d4e424af120247a97bb506be3
hashfunc <root@hashfunc.io>
2023-02-28 23:03:29 +0900

    feat: Add `Problem.P0001`

--- src/Problem/P0001.hs
+++ src/Problem/P0001.hs
@@ -3,4 +3,7 @@ module Problem.P0001
 ) where
 
 solve :: String
-solve = "Problem 1"
+solve = show $ sum $ filter checkCondition [1..1000]
+
+checkCondition :: Int -> Bool
+checkCondition n = n `mod` 3 == 0 || n `mod` 5 == 0
--- src/Selector.hs
+++ src/Selector.hs
@@ -2,8 +2,11 @@ module Selector
 ( select
 ) where
 
+import Problem.P0001
+
 select :: [String] -> IO ()
 select [args] =
     putStrLn $ case args of
+        "1" -> Problem.P0001.solve
         x   -> x ++ " does not exist."
 select _ = putStrLn "Usage: project-euler-hs NUMBER_OF_PROBLEM"
